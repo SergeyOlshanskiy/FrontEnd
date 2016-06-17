cb966a50ff7e36adafe8f8eec5daa346db25c7b8
Sergey Olshanskiy <sergey.olshanskiy@live.com>
2016-06-17 18:16:23 +0300

    update project structure

--- Homework/gulpfile.js
+++ Homework/gulpfile.js
@@ -4,59 +4,61 @@ var clean = require('gulp-clean');
 var cleanCss = require('gulp-clean-css');
 var concatCss = require('gulp-concat-css');
 var inject = require('gulp-inject');
+var htmlmin = require('gulp-htmlmin');
 
 
-gulp.task('default', ['files:copy', 'css:build', 'index']);
+gulp.task('default', ['files:copy', 'css:build', 'html:build']);
 
-gulp.task('index', function () {
-    var target = gulp.src('./private/index.html');
+gulp.task('html:build', ['css:build'], function () {
+    var target = gulp.src('./src/index.html');
     // It's not necessary to read the files (will speed up things), we're only after their paths:
     var sources = gulp.src(['./public/**/*.js', './public/**/*.css'], {read: false});
 
     return target
         .pipe(inject(sources,
             {
-                addPrefix: '..'
+                transform: function (filePath) {
+                    var correctFilePath = filePath.replace('/public', '');
+                    return '<link rel="stylesheet" href="' + correctFilePath + '">';
+                }
             }))
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+            minifyURLs: true
+        }))
         .pipe(gulp.dest('./public'));
 });
 
 gulp.task('files:copy', ['copy-fonts', 'copy-pictures']);
 
-gulp.task('copy-index', function() {
-    return gulp.src('private/index.html')
+gulp.task('copy-index', function () {
+    return gulp.src('src/index.html')
         .pipe(gulp.dest('public'));
 });
 
-gulp.task('copy-fonts', function() {
-    return gulp.src('private/css/fonts/*.*')
-        .pipe(gulp.dest('public/fonts'));
+gulp.task('copy-fonts', function () {
+    return gulp.src('src/css/fonts/*.*')
+        .pipe(gulp.dest('public/css/fonts'));
 });
 
-gulp.task('copy-pictures', function() {
-    return gulp.src('private/pictures/*.*')
+gulp.task('copy-pictures', function () {
+    return gulp.src('src/pictures/*.*')
         .pipe(gulp.dest('public/pictures'));
 });
 
-gulp.task('css:build', ['clean-css', 'concat-css']);
-
-gulp.task('clean', function() {
-    return gulp.src('dist')
-        .pipe(clean({force: true}));
-});
-
-gulp.task('clean-css', function() {
-    return gulp.src('private/css/**/*.css')
+gulp.task('css:build', function () {
+    return gulp.src(['src/css/reset.css', 'src/css/common.css', 'src/css/**/*.css'])
         .pipe(base64({
             extensions: ['png'],
-            maxImageSize: 4*1024*1024
+            maxImageSize: 4 * 1024 * 1024
         }))
+        .pipe(concatCss("style.css"))
         .pipe(cleanCss())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('concat-css', function() {
-    return gulp.src(['dist/reset.css', 'dist/common.css', 'dist/**/*.css'])
-        .pipe(concatCss("style.css"))
-        .pipe(gulp.dest('public/css'));
+gulp.task('clean', function () {
+    return gulp.src('dist')
+        .pipe(clean({force: true}));
 });
